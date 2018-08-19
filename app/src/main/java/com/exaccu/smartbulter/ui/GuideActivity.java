0fbe81aab79d25b335bf832de479f435cd27923b
zailushang1996 <2212926237@qq.com>
2018-08-19 17:22:04 +0800

    readme文件 框架搭建完成 splash页面 guide页面

--- app/src/main/java/com/exaccu/smartbulter/ui/GuideActivity.java
+++ app/src/main/java/com/exaccu/smartbulter/ui/GuideActivity.java
@@ -1,15 +1,79 @@
 package com.exaccu.smartbulter.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.exaccu.smartbulter.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class GuideActivity extends AppCompatActivity {
+
+    @BindView(R.id.mViewPager)
+    ViewPager mViewPager;
+    @BindView(R.id.point1)
+    ImageView point1;
+    @BindView(R.id.point2)
+    ImageView point2;
+    @BindView(R.id.point3)
+    ImageView point3;
+    @BindView(R.id.iv_back)
+    ImageView ivBack;
+
+    private List<View> mList = new ArrayList<>();
+
+    private View view1, view2, view3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+        ButterKnife.bind(this);
+
+        view1 = View.inflate(this, R.layout.pager_item_one, null);
+        view2 = View.inflate(this, R.layout.pager_item_two, null);
+        view3 = View.inflate(this, R.layout.pager_item_three, null);
+        mList.add(view1);
+        mList.add(view2);
+        mList.add(view3);
+
+        mViewPager.setAdapter(new GuideAdapter());
+
+    }
+
+    class GuideAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return mList.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+            return view == object;
+        }
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+            ((ViewPager) container).addView(mList.get(position));
+            return mList.get(position);
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+            ((ViewPager) container).removeView(mList.get(position));
+
+        }
     }
 }
