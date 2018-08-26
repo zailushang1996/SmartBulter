da5ab6c8e5ae73fb5242ad69b46dd0aac42d3110
zailushang1996 <2212926237@qq.com>
2018-08-26 22:49:08 +0800

    gridview

---
+++ app/src/main/java/com/exaccu/smartbulter/adapter/GridAdapter.java
@@ -0,0 +1,80 @@
+package com.exaccu.smartbulter.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.exaccu.smartbulter.R;
+import com.exaccu.smartbulter.entity.GirlData;
+import com.exaccu.smartbulter.utils.PicassoUtils;
+
+import java.util.List;
+
+/**
+ * Author:liuzhixiang
+ * PackageName:com.exaccu.smartbulter.adapter
+ * Create by 17864 on 2018/8/26
+ *
+ * @ Description:
+ */
+public class GridAdapter extends BaseAdapter {
+    private Context mContext;
+    private List<GirlData> mList;
+    private LayoutInflater inflater;
+    private GirlData data;
+    private WindowManager wm;
+    //屏幕宽
+    private int width;
+
+    public GridAdapter(Context mContext, List<GirlData> mList) {
+        this.mContext = mContext;
+        this.mList = mList;
+        inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+        width = wm.getDefaultDisplay().getWidth();
+    }
+
+    @Override
+    public int getCount() {
+        return mList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder = null;
+        if(convertView == null){
+            viewHolder = new ViewHolder();
+            convertView = inflater.inflate(R.layout.girl_item,null);
+            viewHolder.imageView = (ImageView) convertView.findViewById(R.id.imageview);
+            convertView.setTag(viewHolder);
+        }else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        data = mList.get(position);
+        //解析图片
+        String url = data.getImgUrl();
+
+        PicassoUtils.loadImageViewSize(mContext,url,width/2,500,viewHolder.imageView);
+
+        return convertView;
+    }
+
+    class ViewHolder{
+        private ImageView imageView;
+    }
+}
---
+++ app/src/main/java/com/exaccu/smartbulter/entity/GirlData.java
@@ -0,0 +1,22 @@
+package com.exaccu.smartbulter.entity;
+
+/**
+ * Author:liuzhixiang
+ * PackageName:com.exaccu.smartbulter.entity
+ * Create by 17864 on 2018/8/26
+ *
+ * @ Description:
+ */
+public class GirlData {
+
+    //地址
+    private String imgUrl;
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
